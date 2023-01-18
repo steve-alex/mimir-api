506ddb906917a081a19963d7755645caf2d15e13
Steve A <steve.alex@hotmail.co.uk>
2023-01-18 16:11:05 +0000

    feat: connect to db pool

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,10 +2,38 @@ import { Module } from '@nestjs/common';
 import { AppController } from '../controllers/app.controller';
 import { AppService } from '../services/app.service';
 import { UserModule } from './user.module';
+import { MLModule } from './ml.module';
+import { NotionModule } from './notion.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { User } from '../entities/user.entity';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    MLModule,
+    NotionModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [User],
+        synchronize: true,
+        logging: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
