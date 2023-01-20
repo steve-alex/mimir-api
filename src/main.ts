9e391d812e727ae431a96cffd673e9468bd39eef
Steve A <steve.alex@hotmail.co.uk>
2023-01-20 13:00:30 +0000

    feat: enable cors & increase limit for json payloads

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(json({ limit: '5mb' }));
+  // app.use(urlencoded({ extended: true, limit: '100mb' }));
   await app.listen(3000);
 }
 bootstrap();
