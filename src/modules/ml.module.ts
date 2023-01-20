37811c12f4ca17eaf68c8dc63b5b36d424dd8f7c
Steve A <steve.alex@hotmail.co.uk>
2023-01-20 19:00:56 +0000

    feat: commit ml and notion modules

---
+++ src/modules/ml.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { OpenAIService } from '../services/openai.service';
+import { InsightService } from '../services/insight.service';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [OpenAIService, InsightService],
+})
+export class MLModule {}
---
+++ src/modules/notion.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { NotionService } from '../services/notion.service';
+import { NotionController } from '../controllers/notion.controller';
+
+@Module({
+  imports: [],
+  controllers: [NotionController],
+  providers: [NotionService],
+})
+export class NotionModule {}
