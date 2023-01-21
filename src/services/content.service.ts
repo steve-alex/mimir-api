0e929aff4e2f72bb12f93d13435bfdc8abcf41ad
Steve A <steve.alex@hotmail.co.uk>
2023-01-21 20:41:07 +0000

    feat: create content service

--- src/services/content.service.ts
+++ src/services/content.service.ts
@@ -1,58 +1,62 @@
 import { Inject, Injectable } from '@nestjs/common';
-import * as rawBody from 'raw-body';
 import * as cheerio from 'cheerio';
 import { NotionService } from './notion.service';
+import { InsightService } from './insight.service';
+import { NotionPageDetails } from '../types/types';
 
 @Injectable()
 export class ContentService {
-  constructor(@Inject(NotionService) private notionService: NotionService) {}
+  constructor(
+    @Inject(NotionService) private notionService: NotionService,
+    @Inject(InsightService) private insightService: InsightService,
+  ) {}
 
   async createPage(req: any): Promise<void> {
-    const domObject = await this.parseRawHTMLForNotion(req);
-    console.log(
-      '🚀 ~ file: content.service.ts:12 ~ ContentService ~ createPage ~ domObject',
-      domObject,
-    );
-    // await this.storeContent(domObject);
-    await this.notionService.createPage(domObject);
+    const pageDetails = await this.getPageDetails(req.body.html);
+
+    await Promise.all([
+      this.storeContent(pageDetails),
+      this.notionService.createPage({ url: req.body.url, ...pageDetails }),
+    ]);
   }
 
-  async parseRawHTMLForNotion(req: any): Promise<any> {
-    const html = await rawBody(req);
+  async getPageDetails(html: string): Promise<NotionPageDetails> {
+    const { title, content } = await this.parseTextFromHTML(html);
+
+    const [categories, summary] = await Promise.all([
+      this.insightService.categoriseText(content),
+      this.insightService.summariseText(content),
+    ]);
+
+    return { title, categories, summary };
+  }
+
+  async parseTextFromHTML(
+    html: string,
+  ): Promise<{ title: string; content: string }> {
     const $ = cheerio.load(html);
-    const array = [];
+    const htmlElements = $('body')
+      .find('p, h1, h2, h3, h4, h5, h6, li, a, span')
+      .children();
 
-    buildArray(
-      $('body').children().find('p, h1, h2, h3, h4, h5, h6, li, a, span'),
-      array,
-    );
+    const title = $('title').text() || 'Title';
+    let content = '';
 
-    const parsedArray = array
-      .filter(
-        (i) =>
-          i.tag !== 'code' &&
-          i.tag !== 'img' &&
-          i.tag !== 'svg' &&
-          i.tag !== 'div',
-      )
-      .filter((i) => i.text !== '');
+    htmlElements.each((index, element) => {
+      content += extractText($(element));
+    });
 
-    return parsedArray;
+    return { title, content };
 
-    function buildArray(element, tree) {
-      element.children().each(function (i, elem) {
-        const item = {
-          tag: elem.tagName,
-          text: $(this).text(),
-          attributes: elem.attributes,
-          children: [],
-        };
-        tree.push(item);
-        if ($(this).children().length > 0) {
-          item.children = [];
-          buildArray($(this), item.children);
-        }
+    function extractText(element): string {
+      let text = '';
+      if (element.text()) {
+        text += element.text();
+      }
+      element.children().each((index, child) => {
+        text += extractText($(child));
       });
+      return text;
     }
   }
 
