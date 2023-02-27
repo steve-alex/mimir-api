0ef7ab2aa189d25df12dd30d7a829eaeec447355
Steve A <steve.alex@hotmail.co.uk>
2023-02-27 08:18:47 +0000

    feat: update app module with typeorm resources

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,23 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from '../controllers/app.controller';
 import { AppService } from '../services/app.service';
-import { UserModule } from './user.module';
+import { AccountModule } from './account.module';
 import { InisghtsModule } from './insights.module';
 import { NotionModule } from './notion.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
-import { User } from '../entities/user.entity';
+import { Account, AccountRepository } from '../entities/account.entity';
 import { ContentModule } from './content.module';
 import { AllExceptionsFilter } from '../shared/exceptions';
-import PocketBase from 'pocketbase';
+import { AuthModule } from '../auth/auth.module';
+import { Content, ContentRepository } from '../entities/content.entity';
+import {
+  Availability,
+  AvailabilityRepository,
+} from '../entities/availability.entity';
+import { Job, JobRepository } from '../entities/job.entity';
+import { Schedule, ScheduleRepository } from '../entities/schedule.entity';
 
 @Module({
   imports: [
     ContentModule,
-    UserModule,
+    AccountModule,
     InisghtsModule,
     NotionModule,
+    AuthModule,
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
@@ -28,23 +36,29 @@ import PocketBase from 'pocketbase';
         username: configService.get('DB_USERNAME'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
-        entities: [User],
+        entities: [Account, Content, Availability, Job, Schedule],
         synchronize: true,
         logging: true,
       }),
       inject: [ConfigService],
     }),
+    TypeOrmModule.forFeature([
+      AccountRepository,
+      Account,
+      ContentRepository,
+      Content,
+      Availability,
+      AvailabilityRepository,
+      Job,
+      JobRepository,
+      Schedule,
+      ScheduleRepository,
+    ]),
   ],
   controllers: [AppController],
   providers: [
     AppService,
     { provide: AllExceptionsFilter, useClass: AllExceptionsFilter },
-    {
-      provide: 'POCKET_BASE',
-      useFactory: () => {
-        return new PocketBase('http://127.0.0.1:8090');
-      },
-    },
   ],
 })
 export class AppModule {
