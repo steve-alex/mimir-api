dfa9d405b721aea48d924431ca192b8f7f5e0c43
Steve A <steve.alex@hotmail.co.uk>
2023-03-03 08:54:47 +0000

    refactor: move oauth file to calendar folder

---
+++ src/providers/calendar/oauth.entity.ts
@@ -0,0 +1,51 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  EntityRepository,
+  Repository,
+  ManyToOne,
+  CreateDateColumn,
+  UpdateDateColumn,
+  JoinColumn,
+} from 'typeorm';
+import { Account } from '../../models/accounts/account.entity';
+
+export enum OAuthProvider {
+  GoogleCalendar = 'google_calendar',
+}
+
+@Entity({ name: 'oauth' })
+export class OAuth {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Account, (account) => account.id)
+  @JoinColumn({ name: 'accountId' })
+  account: Account;
+
+  @Column({
+    type: 'enum',
+    enum: OAuthProvider,
+    default: OAuthProvider.GoogleCalendar,
+  })
+  provider: OAuthProvider;
+
+  @Column()
+  valid: boolean;
+
+  @Column()
+  access_token: string;
+
+  @Column()
+  refresh_token: string;
+
+  @CreateDateColumn()
+  created_date: Date;
+
+  @UpdateDateColumn()
+  updated_date: Date;
+}
+
+@EntityRepository(OAuth)
+export class OAuthRepository extends Repository<OAuth> {}
