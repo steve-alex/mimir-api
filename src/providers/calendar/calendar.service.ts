143268e926eefcac5ab538b683ba34cac486fc1f
Steve A <steve.alex@hotmail.co.uk>
2023-03-17 15:52:02 +0000

    refactor: move google-api-auth.ts logic to calendar service

--- src/providers/calendar/calendar.service.ts
+++ src/providers/calendar/calendar.service.ts
@@ -1,8 +1,8 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { getToken } from './google-api-auth';
 import { AccountService } from '../../models/accounts/account.service';
 import { CreateEventDetails } from './calendar.entity';
 import { TimeSlot } from '../../types/types';
+import { google } from 'googleapis';
 
 @Injectable()
 export class CalendarService {
@@ -107,8 +107,66 @@ export class CalendarService {
   }
 
   async getValidationUrl() {
-    const url = await getToken();
-    return url;
+    const code = null;
+    const refreshToken = null;
+    const oauth2Client = makeOAuth2Client();
+
+    if (code) {
+      return getRefreshToken(code);
+    } else {
+      return getAuthUrl();
+    }
+
+    async function getAuthUrl() {
+      const url = oauth2Client.generateAuthUrl({
+        // 'online' (default) or 'offline' (gets refresh_token)
+        access_type: 'offline',
+
+        // scopes are documented here: https://developers.google.com/identity/protocols/oauth2/scopes#calendar
+        scope: [
+          'https://www.googleapis.com/auth/calendar',
+          'https://www.googleapis.com/auth/calendar.events',
+        ],
+      });
+
+      return url;
+    }
+
+    async function getRefreshToken(code: string) {
+      const token = await oauth2Client.getToken(code);
+      return token;
+    }
+
+    function makeOAuth2Client() {
+      return new google.auth.OAuth2(
+        process.env.GOOGLE_CALENDAR_CLIENT_ID,
+        process.env.GOOGLE_CALENDAR_CLIENT_SECRET,
+        process.env.GOOGLE_CALENDAR_REDIRECT_URL,
+      );
+    }
+  }
+
+  async swapAuthorizationCodeForTokens(code: string): Promise<oAuthTokens> {
+    const clientId = process.env.GOOGLE_CALENDAR_CLIENT_ID;
+    const clientSecret = process.env.GOOGLE_CALENDAR_CLIENT_SECRET;
+    const redirectUri = process.env.GOOGLE_CALENDAR_REDIRECT_URL;
+    const oauthUrl = 'https://oauth2.googleapis.com/token';
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: `client_id=${clientId}&client_secret=${clientSecret}&code=${code}&redirect_uri=${redirectUri}&grant_type=authorization_code`,
+    };
+
+    const response = await fetch(oauthUrl, options);
+    const data = await response.json();
+
+    const accessToken = data?.access_token;
+    const refreshToken = data?.refresh_token;
+
+    return Object.assign(
+      accessToken && { accessToken },
+      refreshToken && { refreshToken },
+    );
   }
 
   private async getCalendarId(accessToken: string): Promise<string> {
