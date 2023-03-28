3f802cb231d1c071e5dcb5bac9661f6972927418
Steve A <steve.alex@hotmail.co.uk>
2023-03-28 11:45:09 +0100

    fix: JWT signing secret comes from env file

--- src/auth/jwt.auth.ts
+++ src/auth/jwt.auth.ts
@@ -13,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'SECRET_KEY',
+      secretOrKey: process.env.JWT_SIGNING_SECRET,
     });
   }
 
--- src/shared/jwt.ts
+++ src/shared/jwt.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'your-secret-key',
+      secretOrKey: process.env.JWT_SIGNING_SECRET,
     });
   }
 
